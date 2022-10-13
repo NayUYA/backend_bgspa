2b3183fc66e566281d0a396ae33437d5dd153cab
Valeria Leza Sanchez <valerialeza.s@gmail.com>
2022-10-12 20:30:20 -0500

    Prueba

---
+++ src/main/java/org/bgspa/ecommercebg/EcommercebgApplication.java
@@ -0,0 +1,13 @@
+package org.bgspa.ecommercebg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercebgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercebgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bgspa/ecommercebg/EcommercebgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bgspa.ecommercebg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercebgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
