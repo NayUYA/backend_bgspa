f9e0a75a2ac1779187aed4ff4bc6e9134d63ea16
alexanderDelgado <aledt2103@gmail.com>
2022-10-13 22:36:40 -0500

    chores | conexión con Mysql

--- src/main/java/org/bgspa/ecommercebg/model/Usuarios.java
+++ src/main/java/org/bgspa/ecommercebg/model/Usuarios.java
@@ -6,16 +6,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-<<<<<<< HEAD
-=======
+//<<<<<<< HEAD
+//=======
 
 
 @Entity
 @Table(name="usuarios")
->>>>>>> 3bf4c7a04866d9f3fc521899b9facf3c04520e93
-
-@Entity
-@Table(name="usuarios")
+//>>>>>>> 3bf4c7a04866d9f3fc521899b9facf3c04520e93
 public class Usuarios {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY) 
